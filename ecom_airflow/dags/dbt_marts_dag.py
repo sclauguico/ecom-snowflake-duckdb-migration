b61293776d612fb600461f79bfbffdf661b2c16e
Sandy Lauguico <sclauguico@gmail.com>
2025-01-26 18:38:20 +0800

    ADD: marts orchestration

--- ecom_airflow/dags/dbt_marts_dag.py
+++ ecom_airflow/dags/dbt_marts_dag.py
@@ -4,20 +4,20 @@ from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 profile_config = ProfileConfig(
     profile_name="motherduck",
     target_name="prod",
-    profiles_yml_filepath="/usr/local/airflow/dags/dbt/ecom_intermediate/profiles.yml"
+    profiles_yml_filepath="/usr/local/airflow/dags/dbt/ecom_marts/profiles.yml"
 )
 
 execution_config = ExecutionConfig(
     dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt",
 )
 
-intermediate_dag = DbtDag(
-    dag_id="dbt_ecom_intermediate_dag",
+marts_dag = DbtDag(
+    dag_id="dbt_ecom_marts_dag",
     schedule_interval="@daily",
     start_date=datetime(2025, 1, 1),
     catchup=False,
     project_config=ProjectConfig(
-        "/usr/local/airflow/dags/dbt/ecom_intermediate",
+        "/usr/local/airflow/dags/dbt/ecom_marts",
     ),
     profile_config=profile_config,
     execution_config=execution_config,
@@ -29,5 +29,5 @@ intermediate_dag = DbtDag(
         "full_refresh": True,
     },
     max_active_runs=1,
-    tags=['dbt', 'intermediate'],
+    tags=['dbt', 'marts'],
 )
